2cbfee854e527dc4ec80e0624032f0de03a3cdd3
Estevam Alcântara Ponte <tecoponte@gmail.com>
2023-08-11 14:34:02 -0300

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  "ssl": process.env.NODE_ENV === 'production' ? true : false,
+  "ssl": process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   "migrations": [process.env.MIGRATIONS],
   "entities": [process.env.ENTITIES],
   "cli": {
