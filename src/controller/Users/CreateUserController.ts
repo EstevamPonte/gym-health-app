2735af1466879d9a85e1909c1f5fd20bdbfca615
Estevam Alcântara Ponte <tecoponte@gmail.com>
2026-04-26 12:25:44 -0300

    Adicionando coisas de back end

--- src/controller/Users/CreateUserController.ts
+++ src/controller/Users/CreateUserController.ts
@@ -3,14 +3,14 @@ import { CreateUserService } from "../../services/Users/CreateUserService";
 
 class CreateUserController {
   async handle(request: Request, response: Response) {
-    const {name, password} = request.body
+    const { name, password, email } = request.body;
 
-    const cretaeUserService = new CreateUserService()
+    const cretaeUserService = new CreateUserService();
 
-    const user = await cretaeUserService.execute({name, password})
+    const user = await cretaeUserService.execute({ name, password, email });
 
-    return response.json(user)
+    return response.json(user);
   }
 }
 
-export {CreateUserController}
+export { CreateUserController };
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,40 +1,57 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, JoinColumn, OneToOne } from "typeorm";
-import { v4 as uuid } from 'uuid'
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+  OneToOne,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
 import { Exclude } from "class-transformer";
-import { Code } from "./Code"
+import { Code } from "./Code";
 
-@Entity('user')
+@Entity("user")
 class User {
   @PrimaryColumn()
-  readonly id: string
+  readonly id: string;
 
   @Column()
-  name: string
+  email: string;
+
+  @Column()
+  name: string;
 
   @Exclude()
   @Column()
-  password: string
+  password: string;
 
   @CreateDateColumn()
-  created_at: Date
+  created_at: Date;
 
   @UpdateDateColumn()
-  updated_at: Date
+  updated_at: Date;
 
   @Column()
-  code_reference: string
+  code_reference: string;
 
-  @JoinColumn({name: "code_reference"})
-  @OneToOne(() => Code, {
-      onDelete: "CASCADE"
+  @Column("varchar", {
+    array: true,
+    default: "{}",
   })
-  codeReference: Code
+  features: string[];
+
+  @JoinColumn({ name: "code_reference" })
+  @OneToOne(() => Code, {
+    onDelete: "CASCADE",
+  })
+  codeReference: Code;
 
   constructor() {
-    if(!this.id) {
-      this.id = uuid()
+    if (!this.id) {
+      this.id = uuid();
     }
   }
 }
 
-export {User}
+export { User };
